cd9849d157f7f74e8f4cd5ba6ac3c5c8076cc1c5
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-26 22:17:01 -0500

    implemented /mutiply and /divide endpoints

--- 1.Basics/app.py
+++ 1.Basics/app.py
@@ -6,9 +6,16 @@ api = Api(app)
 
 
 def check_posted_data(data, func_name):
-    if func_name == 'add' or func_name == 'subtract':
+    if func_name == 'add' or func_name == 'subtract' or func_name == 'multiply':
         if 'x' not in data or 'y' not in data:
             return 301
+        else:
+            return 200
+    elif func_name == 'divide':
+        if 'x' not in data or 'y' not in data:
+            return 301
+        elif data['y'] == 0:
+            return 302
         else:
             return 200
 
@@ -60,15 +67,55 @@ class Subtract(Resource):
 
 
 class Multiply(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        status_code = check_posted_data(data, 'multiply')
+        if status_code != 200:
+            ret = {
+                "Message": "An error happened",
+                "Status Code": status_code
+            }
+            return ret, status_code
+
+        x = data['x']
+        y = data['y']
+        x = int(x)
+        y = int(y)
+        ret = x*y
+        ret_map = {
+            'Message': ret,
+            'Status Code': status_code
+        }
+        return ret_map, 200
 
 
 class Divide(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        status_code = check_posted_data(data, 'divide')
+        if status_code != 200:
+            ret = {
+                "Message": "An error happened",
+                "Status Code": status_code
+            }
+            return ret, status_code
+
+        x = data['x']
+        y = data['y']
+        x = int(x)
+        y = int(y)
+        ret = (x*1.0)/y
+        ret_map = {
+            'Message': ret,
+            'Status Code': status_code
+        }
+        return ret_map, 200
 
 
 api.add_resource(Add, '/add')
 api.add_resource(Subtract, '/subtract')
+api.add_resource(Multiply, '/multiply')
+api.add_resource(Divide, '/divide')
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5000, debug=True)
