2d47edeba74c6d474954869ace4762fcd73f6887
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-28 21:12:24 -0500

    Changed default port

--- 1.Basics/web/app.py
+++ 1.Basics/web/app.py
@@ -118,4 +118,4 @@ api.add_resource(Multiply, '/multiply')
 api.add_resource(Divide, '/divide')
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", debug=True)
