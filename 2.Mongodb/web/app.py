1166636a5a6609c3cc91bb5d4fa2c44d794c37bd
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-30 10:37:55 -0500

    Added Store resource for adding sentences and token validation

--- 2.Mongodb/web/app.py
+++ 2.Mongodb/web/app.py
@@ -10,6 +10,7 @@ client = MongoClient("mongodb://db:27017")
 db = client.sentencesDB
 users = db['Users']
 
+
 class Register(Resource):
     def post(self):
         posted_data = request.get_json()
@@ -21,7 +22,8 @@ class Register(Resource):
         users.insert_many({
             'Username': username,
             'Password': hashed_pw,
-            'Sentence': ""
+            'Sentence': "",
+            'Tokens': 10
         })
 
         ret_json = {
@@ -32,8 +34,48 @@ class Register(Resource):
         return ret_json, 200
 
 
-api.add_resource(Register, '/register')
+class Store(Resource):
+    def post(self):
+        posted_data = request.get_json()
+        username = posted_data['username']
+        password = posted_data['password']
+        sentence = posted_data['sentence']
 
+        correct_pw = verify_pw(username, password)
+        if not correct_pw:
+            ret_json = {
+                'status': 302,
+                'message': 'Incorrect username or password'
+            }
+            return ret_json, 302
+
+        num_tokens = count_tokens(username)
+        if num_tokens <= 0:
+            ret_json = {
+                'status': 301,
+                'message': 'Not enough tokens'
+
+            }
+            return ret_json, 301
+
+        users.update({
+            "Username": username},
+            {"$set": {
+                "Sentence": sentence,
+                "Tokens": num_tokens - 1
+            }
+            }
+        )
+
+        ret_json = {
+            'status': 200,
+            'message': 'Sentence saved susccesfully'
+        }
+
+        return ret_json, 200
+
+
+api.add_resource(Register, '/register')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
