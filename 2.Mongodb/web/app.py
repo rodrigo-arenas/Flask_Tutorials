b00e4427bd5164216975a452e7a70cdaf0f36e3d
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-29 22:01:36 -0500

    Added '/hello' endpoint

--- 2.Mongodb/web/app.py
+++ 2.Mongodb/web/app.py
@@ -1,25 +1,25 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
-from pymongo import MongoCLient
+from pymongo import MongoClient
 
 app = Flask(__name__)
 api = Api(app)
 # The client db name must match docker-compose db service
-client = MongoCLient("mongodb://db:27017")
+client = MongoClient("mongodb://db:27017")
 db = client.aNewDB
 UserNum = db["UserNum"]
 UserNum.insert({
-    "num_of_users":0
+    "num_of_users": 0
 })
-
 
 
 class Visit(Resource):
     def get(self):
         prev_num = UserNum.find({})[0]['num_of_users']
         new_num = prev_num + 1
-        UserNum.update({}, {$set:{"num_of_users":new_num}})
-        return str("Hello user "+ str(new_num))
+        UserNum.update({}, {"$set": {"num_of_users": new_num}})
+        return str("Hello user " + str(new_num))
+
 
 def check_posted_data(data, func_name):
     if func_name == 'add' or func_name == 'subtract' or func_name == 'multiply':
@@ -138,6 +138,7 @@ api.add_resource(Add, '/add')
 api.add_resource(Subtract, '/subtract')
 api.add_resource(Multiply, '/multiply')
 api.add_resource(Divide, '/divide')
+api.add_resource(Visit, '/hello')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
