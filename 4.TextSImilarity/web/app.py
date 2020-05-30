1b3875956d1d7d2f666bcfa8bda1d9b489f7f77a
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-30 18:12:28 -0500

    Implemented verify_pw, current tokens functions. Implemented Detect class endpoint

--- 4.TextSImilarity/web/app.py
+++ 4.TextSImilarity/web/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
-import bycript
-
+import bcrypt
+import spacy
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,6 +19,22 @@ def user_exists(username):
         return True
 
 
+def verify_pw(username, password):
+    if not user_exists(username):
+        return False
+
+    hashed_pw = users.find({"Username": username})[0]["Password"]
+    if bcrypt.hashpw(password.encode('uft8'), hashed_pw) == hashed_pw:
+        return True
+    else:
+        return False
+
+
+def count_tokens(username):
+    tokens = users.find({"Username": username})[0]["Tokens"]
+    return tokens
+
+
 class Register(Resource):
     def post(self):
         posted_data = request.get_json()
@@ -32,7 +48,7 @@ class Register(Resource):
             }
             return ret_json, 301
 
-        hash_pw = bycript.hashpw(password.encode('utf8'), bycript.gensalt())
+        hash_pw = bcrypt.hashpw(password.encode('utf8'), bycript.gensalt())
 
         users.insert_one({
             "Username": username,
@@ -44,6 +60,61 @@ class Register(Resource):
             'status': 200,
             'message': "You've successfully signed up to the API"
         }
+        return ret_json, 200
+
+
+class Detect(Resource):
+    def post(self):
+        posted_data = request.get_json()
+        username = posted_data["username"]
+        password = posted_data["password"]
+        text1 = posted_data["text1"]
+        text2 = posted_data["text2"]
+
+        if not user_exists(username):
+            ret_json = {
+                'status': 301,
+                'message': "Invalid username"
+            }
+            return ret_json, 301
+
+        correct_pw = verify_pw(username, password)
+        if not correct_pw:
+            ret_json = {
+                'status': 302,
+                'message': 'Invalid username or password'
+            }
+
+        num_tokens = count_tokens(username)
+        if num_tokens <=0:
+            ret_json = {
+                'status': 303,
+                'message': "out of tokens"
+            }
+            return num_tokens, 303
+
+        nlp = spacy.load('en_core_web_sm')
+
+        text1 = nlp(text1)
+        text2 = nlp(text2)
+
+        ratio = text1.similarity(text2)
+
+        ret_json = {
+            'status': 200,
+            'similarity': "similarity score calculated"
+            'score': ratio
+        }
+
+        current_tokens = count_tokens(username)
+        users.update(
+            {
+                "Username": username,
+                {
+                    "$set": {"Tokens": current_tokens-1}
+                }
+            })
+
         return ret_json, 200
 
 
