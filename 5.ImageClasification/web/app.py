7ebf9af5ba2a09fe69d76d640e6ccf8b7a4e2958
rodrigoarenas <rodrigo.arenas456@outlook.com>
2020-05-31 10:18:33 -0500

    Set up proyect basic structure

---
+++ 5.ImageClasification/web/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import bcrypt
+import numpy as np
+import requests
+import subprocess
+import json
+
